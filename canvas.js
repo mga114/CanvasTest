d3683a2c3b6f5d5759c5e08ef46312ddfc78e3f9
mga114 <84060777+mga114@users.noreply.github.com>
2022-01-12 15:29:04 +1300

    updated documentation

--- canvas.js
+++ canvas.js
@@ -1,3 +1,4 @@
+//HTML elements to JS
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 const scoreElement = document.querySelector("#scoreElement");
@@ -5,18 +6,21 @@ const startButton = document.querySelector("#startButton");
 const menuUI = document.querySelector("#menuUI");
 const menuScoreElement = document.querySelector("#menuScoreElement");
 
-//Resizing
+//Resizing canvas to fit window size
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+//Ensures scales and positionining stays consistent when window is resized
 window.addEventListener('resize', () => {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
+    //centers player
     player.x = canvas.width / 2;
     player.y = canvas.height / 2;
 });
 
 
+//Base class entity for all rendered objects in the canvas
 class Entity {
     constructor (x, y, radius, colour, velocity) {
         this.x = x;
@@ -27,6 +31,7 @@ class Entity {
     }
 
     draw() {
+        //Draws each entity as a circle
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
         context.fillStyle = this.colour;
@@ -34,6 +39,7 @@ class Entity {
     }
 
     update() {
+        //draws and moves the entity
         this.draw();
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
@@ -60,7 +66,7 @@ class Enemy extends Entity{
     }
 }
 
-const friction = 0.99;
+const friction = 0.99; //amount the particles slows down by, 0-1, closer to 0 means higher friction
 class Particle extends Entity{ 
     constructor (x, y, radius, colour, velocity) {
         super(x, y, radius, colour, velocity);
@@ -69,6 +75,7 @@ class Particle extends Entity{
     }
 
     draw () {
+        //renders particles at different alpha values to fade out
         context.save();
         context.globalAlpha = this.alpha;
         context.beginPath();
@@ -88,12 +95,14 @@ class Particle extends Entity{
     }
 }
 
+//initialising variables
 let player;
 let score;
 let projectiles;
 let enemies;
 let particles;
 
+//called at the start of each game, resets/initialises game variables
 function init() {
     player = new Player(canvas.width / 2, canvas.height / 2, 30, 'white', {x: 0, y: 0});
     score = 0;
@@ -103,37 +112,45 @@ function init() {
     scoreElement.innerHTML = 0;
 }
 
+//spawns enemies using setInterval function where the timing is controlled by second argument of setInterval()
 function spawnEnemies () {
     setInterval(() => {
-        const radius = Math.random() * 30 + 6;
+        const radius = Math.random() * 30 + 6; //radius of each enemy
         let x;
         let y;
 
+        //sets spawn location of the enemy
         if (Math.random() < 0.5) {
+            //set position to somewhere on the left or right of the screen
             x = Math.random() < 0.5 ? -radius : canvas.width + radius;
             y = Math.random() * canvas.height;
         } else {
+            //set position to somewhere on the top or bottom of the screen
             x = Math.random() * canvas.width;
             y = Math.random() < 0.5 ? -radius : canvas.width + radius;
         }
         
-        const colour = `hsl(${Math.random() * 360}, 50%, 50%)`;
-        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
-        const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
+        const colour = `hsl(${Math.random() * 360}, 50%, 50%)`; //random colour value
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x); //computes angle to player
+        const velocity = {x: Math.cos(angle), y: Math.sin(angle)}; //velocity needed to get to the player
         enemies.push (new Enemy (x, y, radius, colour, velocity));
-    }, (Math.random() + 0.8) * 1000);
+    }, (Math.random() + 0.8) * 1000); //change this value to change spawn rate
 }
 
+//tests if a projectile is out of the range of the screen
 function projectileOutOfBounds (projectile) {
     return projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 || projectile.y + projectile.radius > canvas.height;
 }
 
-let animationID;
+let animationID; //current animation frame ID
 
+//main game loop
 function animate() {
-    animationID = requestAnimationFrame(animate);
+    animationID = requestAnimationFrame(animate); //key function responsible for game loop
+    //resets screen after each frame
     context.fillStyle = 'rgba(0, 0, 0, 0.1)';
     context.fillRect(0, 0, canvas.width, canvas.height);
+    //updates game components
     updateParticles();
     updateProjectiles();
     updateEnemies();
@@ -142,6 +159,7 @@ function animate() {
 
 function updateParticles () {
     particles.forEach((particle, index) => {
+        //if particle is invisble then delete
         if (particle.alpha <= 0) {
             particles.splice(index, 1);
         } else {
@@ -155,38 +173,49 @@ function updateProjectiles () {
         projectile.update();
         if (projectileOutOfBounds(projectile)) {
             setTimeout(() => {
+                //delete projectile
                 projectiles.splice(index, 1);
             }, 0);
         }
     });
 }
 
+
 function updateEnemies () {
     enemies.forEach((enemy, enemyIndex) => {
         enemy.update();
-
+        
+        //distance between enemy and player
         const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
         if (dist - enemy.radius - player.radius < 1) {
+            //game over, enemy touching player
             cancelAnimationFrame(animationID);
-            menuUI.style.display = 'flex';
+            menuUI.style.display = 'flex'; //shows menuUI
             menuScoreElement.innerHTML = score;
         }
 
         projectiles.forEach((projectile, projIndex) => {
+            //test if any projectile is touching the enemy
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
-            if (dist - projectile.radius - enemy.radius < 1) {
+            if (dist - projectile.radius - enemy.radius < 1) { 
+                //projectile touched enemy
 
+                //create particle explosion
                 for (let i = 0; i < enemy.radius * 2; i++) {
                     particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.colour, {x: (Math.random() - 0.5) * (Math.random() * 5), y: (Math.random() - 0.5) * (Math.random() * 5)}));
                 }
 
+                //if enemy is too large
                 if(enemy.radius - 10 > 5) {
-                    gsap.to(enemy, {radius: enemy.radius - 10});
+                    //shrink enemy
+                    gsap.to(enemy, {radius: enemy.radius - 10}); //using GSAP to lerp size of enemy
                     setTimeout(() => {
+                        //remove projectile
                         projectiles.splice(projIndex, 1);
                     }, 0);
                     score += 100;
                 } else {
+                    //remove both projectile and enemy
                     setTimeout(() => {
                         enemies.splice(enemyIndex, 1);
                         projectiles.splice(projIndex, 1);
@@ -200,12 +229,15 @@ function updateEnemies () {
     });
 }
 
+//shoot projectile
 window.addEventListener('click', (event) => {
+    //angle and velocity relative to mouse click from player position
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = {x: Math.cos(angle) * 6, y: Math.sin(angle) * 6};
     projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity));
 });
 
+//starts/restarts game
 startButton.addEventListener('click', () => {
     init(); 
     animate();
